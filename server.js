78c8367e20abfe98c16e31e62bd1ad14c47926a0
VanshVala <102245923+Vanshvala23@users.noreply.github.com>
2026-01-13 14:35:24 +0530

    Updated

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express=require('express');
 const app=express();
 const connectDB=require('./config/db');
+const cors = require("cors")
 
 connectDB();
+const cors = require("cors");
+
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://your-frontend-domain.onrender.com"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 
 app.use(express.json());
 
